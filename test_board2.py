bb9207915cccdb7048aaea1e4514a6ccf9bff859
ankit.patnaik@hashedin.com <ankit.patnaik@hashedin.com>
2019-02-12 11:15:02 +0530

    test case_43

--- test_board2.py
+++ test_board2.py
@@ -11,10 +11,15 @@ class Test_board_push2:
             cls.url = data['url']
             cls.name = data['uname']
             cls.payload = {'key': cls.key, 'token': cls.token, 'name': cls.name}
-    def test_update_board(self):
+    def test_update_board(self):#testcase_41
         id="5c610bdaef382018a436b32e"
         response=requests.put(self.url+id,params={'key':self.key,'token':self.token,'name':'divya hg'})
         assert (response.status_code==200)
         name=requests.get(self.url+'/'+id,params=self.payload).json()['name']
         assert (name=='divya hg')
+    def test_update_board_INVALID_KEY(self):#testcase_41
+        id="5c610bdaef382018a436b32e"
+        response=requests.put(self.url+id,params={'key':'dscs','token':self.token,'name':'divya hg'})
+        assert (response.status_code==401)
+
 
