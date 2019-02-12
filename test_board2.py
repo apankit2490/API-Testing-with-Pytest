32c75223443ce4be34e9779d40dab686197a837d
ankit.patnaik@hashedin.com <ankit.patnaik@hashedin.com>
2019-02-12 11:42:10 +0530

    test case_45

--- test_board2.py
+++ test_board2.py
@@ -17,20 +17,28 @@ class Test_board_push2:
         assert (response.status_code==200)
         name=requests.get(self.url+'/'+id,params=self.payload).json()['name']
         assert (name=='divya hg')
-    def test_update_board_INVALID_KEY(self):#testcase_41
+    def test_update_board_INVALID_KEY(self):#testcase_42
         id="5c610bdaef382018a436b32e"
         response=requests.put(self.url+id,params={'key':'dscs','token':self.token,'name':'divya hg'})
         assert (response.status_code==401)
 
-    def test_update_board_INVALID_boardid(self):  # testcase_41
+    def test_update_board_INVALID_boardid(self):  # testcase_43
         id = "aasskk"
         response = requests.put(self.url + id, params={'key': self.key, 'token': self.token, 'name': 'divya hg'})
         assert (response.status_code == 400)
 
-    def test_update_board(self):#testcase_41
+    def test_update_board(self):#testcase_44
         id="5c616913e3fca870309fd37a"
         response=requests.put(self.url+id,params={'key':self.key,'token':self.token,'name':'divya hg'})
         assert (response.status_code==401)
+    def test_update_boardmember(self):#testcase_44
+        id="5c610bdaef382018a436b32e"
+        response=requests.put(self.url+id+'/members',params={'key':self.key,'token':self.token,'fullName':'ankkit249012',
+                                                             'email':'abcd@xyz.com','type':'normal'})
+        assert (response.status_code==200)
+
+
+
 
 
 
